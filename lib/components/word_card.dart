5617ecda02a99c2e3a6cae375fb1cd67a20a66ae
Alexander <valverin95@gmail.com>
2023-03-11 14:06:29 +0300

    update word card

--- lib/components/word_card.dart
+++ lib/components/word_card.dart
@@ -13,7 +13,7 @@ class WordCard extends StatelessWidget {
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
 
-    var textStyle = theme.textTheme.displayMedium!.copyWith(
+    var style = theme.textTheme.displayMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
       fontWeight: FontWeight.w900,
     );
@@ -23,12 +23,31 @@ class WordCard extends StatelessWidget {
       elevation: 12,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(
-          word.asCamelCase,
-          style: textStyle,
-          semanticsLabel: "${word.first} ${word.second}",
+        child: AnimatedSize(
+          duration: Duration(milliseconds: 200),
+          // Make sure that the compound word wraps correctly when the window
+          // is too narrow.
+          child: MergeSemantics(
+            child: Wrap(
+              children: [
+                Text(
+                  upperFirstLetter(word.first),
+                  style: style.copyWith(fontWeight: FontWeight.w200, letterSpacing: -2),
+                ),
+                Text(
+                  upperFirstLetter(word.second),
+                  style: style.copyWith(fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
+}
+
+String upperFirstLetter(String word) {
+  var letterList = word.split("");
+  return letterList.first.toUpperCase() + letterList.sublist(1).join("");
 }
